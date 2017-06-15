8f49f1bd7b0b8eec8b56b2075eef36161d06302c
Richard Tu <tu.r@husky.neu.edu>
2017-06-15 12:40:04 -0400

    changed extend

--- code/src/models/MySQLModel.java
+++ code/src/models/MySQLModel.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 /**
  * Model where the MySQL Server is hosted locally.
  */
-public class MySQLModel implements AModel {
+public class MySQLModel extends AModel {
     @Override
     public ArrayList<ArrayList<String>> getTopThree(ResultSet results) {
         return null;
