2670f4296890b2dbe9c2d83c85e04a378d0c7ab4
Richard Tu <tu.r@husky.neu.edu>
2017-06-21 00:55:58 -0400

    SQL Update

--- TimeToClimbDatabase.sql
+++ TimeToClimbDatabase.sql
@@ -83,12 +83,15 @@ INSERT INTO leagues VALUES
 
 INSERT INTO champions VALUES
 	(1, "Wukong", "tank"),
-	(2, "Malphite", "tank");
+	(2, "Malphite", "tank"),
+    (3, "Lux", "mage");
 
 INSERT INTO matches VALUES
-	(1, 5, "tank", "top", true, 10, 2, 5, 135, 1, 1),
-	(2, 5, "tank", "top", true, 10, 2, 5, 135, 2, 1),
-	(3, 5, "tank", "top", false, 20, 3, 10, 135, 2, 1);
+	(1, 5, "tank", "top", true, 5, 5, 5, 135, 1, 1),
+	(2, 5, "tank", "top", true, 6, 6, 0, 135, 2, 1),
+	(4, 5, "mage", "mid", true, 20, 6, 8, 180, 3, 2),
+	(5, 5, "tank", "top", true, 8, 8, 4, 80, 2, 2),
+	(3, 5, "tank", "top", false, 7, 7, 0, 135, 2, 1);
 
 -- Procedures --
 -- retrieve_summoner -- 
@@ -141,25 +144,28 @@ DELIMITER $$
 CREATE PROCEDURE
 	top_three_top(IN sum_id BIGINT)
 	BEGIN
+    SELECT
+		champion_name,
+		kda,
+		winrate,
+		kda * winrate AS performance 
+	FROM ( 
 		SELECT
-			champions.champion_name,
-			(SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths) AS kda,
-			count(matches.match_win)                                                                        AS games_played,
-			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS winrate,
-			((SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths))/
-            count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS performance,
-            -- STDDEV still needs fixing
-			STDDEV(0)                                                                AS standard_deviation
-		FROM matches
-			INNER JOIN champions ON matches.champion_id = champions.champion_id
-		WHERE match_lane = 'TOP'
-		GROUP BY champion_name -- Where do we select top 3? Right now, it's just ordered alphabetically
-        ORDER BY performance DESC
-		LIMIT 3;
+			champions.champion_name 																			AS champion_name,
+			(SUM(matches.match_kills) + SUM(matches.match_assists)) / SUM(matches.match_deaths)					AS kda,
+			count(matches.match_win)                                                                       		AS games_played,
+			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                         		AS winrate
+		FROM matches INNER JOIN champions ON matches.champion_id = champions.champion_id
+		WHERE match_lane = 'TOP' AND matches.summoner_id = sum_id
+		GROUP BY champion_name
+		) AS calculations
+	ORDER BY performance DESC
+	LIMIT 3;
 	END$$
 DELIMITER ;
 
 CALL top_three_top(1);
+CALL top_three_top(2);
 
 -- top_three_jng --
 -- Gets the top 3 champions the summoner played in the jungle
@@ -170,20 +176,23 @@ CREATE PROCEDURE
 	IN sum_id BIGINT
 )
 	BEGIN
+    SELECT
+		champion_name,
+		kda,
+		winrate,
+		kda * winrate AS performance 
+	FROM ( 
 		SELECT
-			champions.champion_name,
-			(SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths) AS kda,
-			count(matches.match_win)                                                                        AS games_played,
-			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS winrate,
-			((SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths))/
-            count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS performance,
-            -- STDDEV still needs fixing
-			STDDEV(0)                                                                AS standard_deviation
-		FROM matches
-			INNER JOIN champions ON matches.champion_id = champions.champion_id
-		WHERE match_lane = 'JUNGLE'
+			champions.champion_name 																			AS champion_name,
+			(SUM(matches.match_kills) + SUM(matches.match_assists)) / SUM(matches.match_deaths)					AS kda,
+			count(matches.match_win)                                                                       		AS games_played,
+			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                         		AS winrate
+		FROM matches INNER JOIN champions ON matches.champion_id = champions.champion_id
+		WHERE match_lane = 'JNG' AND matches.summoner_id = sum_id
 		GROUP BY champion_name
-		LIMIT 3;
+		) AS calculations
+	ORDER BY performance DESC
+	LIMIT 3;
 	END$$
 DELIMITER ;
 
@@ -198,24 +207,28 @@ CREATE PROCEDURE
 	IN sum_id BIGINT
 )
 	BEGIN
+    SELECT
+		champion_name,
+		kda,
+		winrate,
+		kda * winrate AS performance 
+	FROM ( 
 		SELECT
-			champions.champion_name,
-			(SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths) AS kda,
-			count(matches.match_win)                                                                        AS games_played,
-			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS winrate,
-			((SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths))/
-            count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS performance,
-            -- STDDEV still needs fixing
-			STDDEV(0)                                                                AS standard_deviation
-		FROM matches
-			INNER JOIN champions ON matches.champion_id = champions.champion_id
-		WHERE match_lane = 'MID'
+			champions.champion_name 																			AS champion_name,
+			(SUM(matches.match_kills) + SUM(matches.match_assists)) / SUM(matches.match_deaths)					AS kda,
+			count(matches.match_win)                                                                       		AS games_played,
+			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                         		AS winrate
+		FROM matches INNER JOIN champions ON matches.champion_id = champions.champion_id
+		WHERE match_lane = 'MID' AND matches.summoner_id = sum_id
 		GROUP BY champion_name
-		LIMIT 3;
+		) AS calculations
+	ORDER BY performance DESC
+	LIMIT 3;
 	END$$
 DELIMITER ;
 
 CALL top_three_mid(1);
+CALL top_three_mid(2);
 
 -- top_three_adc --
 -- Gets the top 3 champions the summoner played in the bottom lane as adc
@@ -226,20 +239,23 @@ CREATE PROCEDURE
 	IN sum_id BIGINT
 )
 	BEGIN
+    SELECT
+		champion_name,
+		kda,
+		winrate,
+		kda * winrate AS performance 
+	FROM ( 
 		SELECT
-			champions.champion_name,
-			(SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths) AS kda,
-			count(matches.match_win)                                                                        AS games_played,
-			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS winrate,
-			((SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths))/
-            count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS performance,
-            -- STDDEV still needs fixing
-			STDDEV(0)                                                                AS standard_deviation
-		FROM matches
-			INNER JOIN champions ON matches.champion_id = champions.champion_id
-		WHERE match_lane = 'BOTTOM' AND match_role = 'DUO_CARRY'
+			champions.champion_name 																			AS champion_name,
+			(SUM(matches.match_kills) + SUM(matches.match_assists)) / SUM(matches.match_deaths)					AS kda,
+			count(matches.match_win)                                                                       		AS games_played,
+			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                         		AS winrate
+		FROM matches INNER JOIN champions ON matches.champion_id = champions.champion_id
+		WHERE match_lane = 'BOT' AND match_role = 'DUO_CARRY' AND matches.summoner_id = sum_id
 		GROUP BY champion_name
-		LIMIT 3;
+		) AS calculations
+	ORDER BY performance DESC
+	LIMIT 3;
 	END$$
 DELIMITER ;
 
@@ -254,20 +270,23 @@ CREATE PROCEDURE
 	IN sum_id BIGINT
 )
 	BEGIN
+    SELECT
+		champion_name,
+		kda,
+		winrate,
+		kda * winrate AS performance 
+	FROM ( 
 		SELECT
-			champions.champion_name,
-			(SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths) AS kda,
-			count(matches.match_win)                                                                        AS games_played,
-			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS winrate,
-			((SUM(ALL matches.match_kills) + SUM(ALL matches.match_assists)) / SUM(ALL matches.match_deaths))/
-            count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                          AS performance,
-            -- STDDEV still needs fixing
-			STDDEV(0)                                                                AS standard_deviation
-		FROM matches
-			INNER JOIN champions ON matches.champion_id = champions.champion_id
-		WHERE match_lane = 'BOTTOM' AND match_role = 'DUO_SUPPORT'
+			champions.champion_name 																			AS champion_name,
+			(SUM(matches.match_kills) + SUM(matches.match_assists)) / SUM(matches.match_deaths)					AS kda,
+			count(matches.match_win)                                                                       		AS games_played,
+			count(CASE WHEN matches.match_win THEN 1 END)/count(matches.match_win)                         		AS winrate
+		FROM matches INNER JOIN champions ON matches.champion_id = champions.champion_id
+		WHERE match_lane = 'BOT' AND match_role = 'DUO_SUPPORT' AND matches.summoner_id = sum_id
 		GROUP BY champion_name
-		LIMIT 3;
+		) AS calculations
+	ORDER BY performance DESC
+	LIMIT 3;
 	END$$
 DELIMITER ;
 
